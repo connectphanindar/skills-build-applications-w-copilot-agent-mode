c2626de7da280d51aca51b2e1a15525f9b4c4a6d
connectphanindar <connectphanindar@gmail.com>
2026-05-19 04:41:40 +0000

    Added Setup Django REST Framework and test the REST API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-t2$1j=blz6eg++%*0ujhd)^2+290^5)b5i1j&f)z_mkuu9twa_
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow codespace and localhost
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    ALLOWED_HOSTS = [f'{codespace_name}-8000.app.github.dev', 'localhost', '127.0.0.1']
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
